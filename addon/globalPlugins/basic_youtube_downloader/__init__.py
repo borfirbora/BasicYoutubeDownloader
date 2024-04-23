0cca774f27435ae5097962c4e8ddcbc155e11afc
borfirbora <borafirlangec@gmail.com>
2024-04-23 13:59:53 +0300

    added onComplette and onProgress callbacks for downloading videos and playlists.

--- addon/globalPlugins/basic_youtube_downloader/__init__.py
+++ addon/globalPlugins/basic_youtube_downloader/__init__.py
@@ -24,4 +24,39 @@ class GlobalPlugin(GlobalPlugin):
 		"Basit Youtube İndirici"
 	), gesture="kb:NVDA+shift+y")
 	def script_checkLink(self, gesture):
-		gui.speech.speakMessage("deneme mesajıdır bu. Bakalım başarılı olacak mı?")
+		try:
+			url = api.getClipData()
+			self.recogniseLink(url)
+			self.askQuality()
+		except OSError:
+			gui.speech.speakMessage("Panonuz boş")
+
+	def recogniseLink(self, link: str):
+		if link.startswith("https://www.youtube.com/watch?"):
+			gui.speech.speakMessage("Bu mükemmel bir Youtube linki.")
+		elif link.startswith("https://www.youtube.com/playlist?"):
+			gui.speech.speakMessage("Bu Mükemmel bir Çalma Listesi linki.")
+		else:
+			gui.speech.speakMessage("geçerli bir link değil...")
+
+	def askQuality(self):
+		def askGUI():
+			dialog = wx.SingleChoiceDialog(None,_(
+				# Translators: Asking for quality.
+			"Video kalitenizi seçin."),_(
+				# Translators: The Window title.
+			"Kalite seçin"),[_("En yüksek kalite"),_("En düşük kalite")])
+			if dialog.ShowModal() == wx.ID_OK:
+				self.quality = dialog.GetSelection()
+		wx.CallAfter(askGUI)
+
+	def onProgress(self, stream, chunk,bytes_remaining):
+		total_size = stream.filesize
+		bytes_downloaded = total_size - bytes_remaining
+		percent = (bytes_downloaded / total_size) * 100
+		self.percent = f"{percent:.0f}% indirildi"
+
+	def onComplete(self, stream, filepath):
+		gui.speech.speakMessage(_(f"Dosya, {filepath} konumuna indirildi.."))
+
+	
